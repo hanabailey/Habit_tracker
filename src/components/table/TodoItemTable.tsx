ba0a6763ca10a197819cd0bbc0d6e214d6bb5c5a
HANA CHOI <hana40820@gmail.com>
2023-03-20 16:01:57 +0900

    Adding luxon date and tweak days code

--- src/components/table/TodoItemTable.tsx
+++ src/components/table/TodoItemTable.tsx
@@ -1,18 +1,24 @@
 import { text } from 'stream/consumers';
 import styles from './TodoItemTable.module.css'
+import { DateTime } from "luxon";
+import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
 
 function TodoItemTable(){
+  const [month, setMonth] = useState(DateTime.now().month);
+  const daysInMonth = DateTime.local(month).daysInMonth;
 
   //30치 같은 칸을 만드려고 할때 반복문으로 만들어서 돌릴 수 있음.
   const headcols = [];
-  for(let i=0; i< 30; i++){
+  for(let i=0; i< daysInMonth; i++){
     headcols.push(<th className={styles.th} key={i}>{i+1}</th>)
   }
 
   const cells =[];
-  for(let i=0; i<30; i++){
-    cells.push(<td className={styles.td} key = {i}>{''}</td>)
+  for(let i=0; i< daysInMonth; i++){
+    cells.push(<td className={styles.td} key = {i}><span onClick={}>{<FontAwesomeIcon icon={faCircle}/> }</span></td>)
   }
 
   return(
